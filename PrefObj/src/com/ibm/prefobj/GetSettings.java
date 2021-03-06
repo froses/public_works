2a7ca81da96d130da0ca6b9b9ace7f875782e0f4
francesc <francesc@10.0.0.26>
2021-03-06 17:16:00 +0100

    Javadoc & fromat code.

--- PrefObj/src/com/ibm/prefobj/GetSettings.java
+++ PrefObj/src/com/ibm/prefobj/GetSettings.java
@@ -5,16 +5,23 @@ package com.ibm.prefobj;
 import java.awt.*;
 import java.util.prefs.*;
 
-public class GetSettings
-{
-  static public void main( String args[] ) throws Exception {
-    java.lang.Class clazz = GetSettings.class;
-    Preferences prefs = Preferences.userNodeForPackage( clazz );
+/**
+ * <p>
+ * Sample class showing how to recover a Java Object previously saved in 
+ * a Preferences node.
+ * </p>
+ * @author francesc
+ *
+ */
+public class GetSettings {
+	static public void main(String args[]) throws Exception {
+		java.lang.Class clazz = GetSettings.class;
+		Preferences prefs = Preferences.userNodeForPackage(clazz);
 
-    Font font = (Font)PrefObj.getObject( prefs, "font" );
-    Color color = (Color)PrefObj.getObject( prefs, "color" );
+		Font font = (Font) PrefObj.getObject(prefs, "font");
+		Color color = (Color) PrefObj.getObject(prefs, "color");
 
-    System.out.println( "Read "+font );
-    System.out.println( "Read "+color );
-  }
+		System.out.println("Read " + font);
+		System.out.println("Read " + color);
+	}
 }
--- PrefObj/src/com/ibm/prefobj/SetSettings.java
+++ PrefObj/src/com/ibm/prefobj/SetSettings.java
@@ -5,18 +5,24 @@ package com.ibm.prefobj;
 import java.awt.*;
 import java.util.prefs.*;
 
-public class SetSettings
-{
-  static public void main( String args[] ) throws Exception {
-    Preferences prefs = Preferences.userNodeForPackage( SetSettings.class );
+/**
+ * <p>
+ * Sample class of how persist Java Objects into a Preferences node.
+ * </p>
+ * @author francesc
+ *
+ */
+public class SetSettings {
+	static public void main(String args[]) throws Exception {
+		Preferences prefs = Preferences.userNodeForPackage(SetSettings.class);
 
-    Color color = new Color( 0xff, 0xcc, 0x99 );
-    Font font = new Font( "Courier", Font.PLAIN, 24 );
+		Color color = new Color(0xff, 0xcc, 0x99);
+		Font font = new Font("Courier", Font.PLAIN, 24);
 
-    PrefObj.putObject( prefs, "font", font );
-    PrefObj.putObject( prefs, "color", color );
+		PrefObj.putObject(prefs, "font", font);
+		PrefObj.putObject(prefs, "color", color);
 
-    System.out.println( "Wrote "+font );
-    System.out.println( "Wrote "+color );
-  }
+		System.out.println("Wrote " + font);
+		System.out.println("Wrote " + color);
+	}
 }
